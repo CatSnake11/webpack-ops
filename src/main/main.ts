82a4df2da1e8bee7ab8c4d227c231fa50a61c0dc
Dave Marquess <dave.marquess@gmail.com>
2018-12-31 18:57:16 -0800

    add zoomable sunburst

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,7 @@ console.log(now)
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
-let generate1 = async function generateStats () {
+let generate1 = async function generateStats() {
   const stats = await exec("rimraf dist && webpack --watch --config ./webpack.dev.js --progress --colors --profile --json > webpack-stats.json")
   return { stats }
 };
@@ -99,7 +99,7 @@ ipcMain.on('read-config', (event: any, configNumber: any) => {
   // after package.json is loaded configs have been sent to renderer and user
   // has now selected one and we need to load
   console.log("on load-config")
-  console.log("use configuration: ", configNumber) 
+  console.log("use configuration: ", configNumber)
 
   readConfig(configNumber)
 })
@@ -182,7 +182,7 @@ function readConfig(entry: number) {
   console.log("User selected entry", entry)
   console.log(`selecting ${entry ? "1st" : "second"} configuration.\n`);
 
-  let config = listOfConfigs[entry].split("--config" )[1].trimLeft().split(" ")[0]
+  let config = listOfConfigs[entry].split("--config")[1].trimLeft().split(" ")[0]
   console.log("loading webpack config", config)
   fs.readFile(config, (err, data) => {
     if (err) {
@@ -220,21 +220,21 @@ function parseConfig(entry: string, filepath: string) {
   // console.log(obj.body[obj.body.length-1].expression.left.object.name)
   // console.log(obj.body[obj.body.length-1].expression.left.property.name)
   let body = ast.body;
-  console.log(body[body.length-1].expression.left.object.name)  // should be module
-  console.log(body[body.length-1].expression.left.property.name)  // should be exports
+  console.log(body[body.length - 1].expression.left.object.name)  // should be module
+  console.log(body[body.length - 1].expression.left.property.name)  // should be exports
 
   // todo: if the last element is module.exports, which it should be, if it's an Object
   // we have found the config object. If it's an array, we need to find the config objects.
 
   // finding the config objects
-  let configNames = body[body.length-1].expression.right.elements;
+  let configNames = body[body.length - 1].expression.right.elements;
   let configs = [];
 
-  for (let i=0; i<configNames.length; i++) {
+  for (let i = 0; i < configNames.length; i++) {
     console.log(configNames[i].name);
     let config;
     try {
-      config = body.filter( (d: any) => {
+      config = body.filter((d: any) => {
         return (
           d.type === "VariableDeclaration" &&
           d.declarations[0].id.name === configNames[i].name
@@ -243,7 +243,7 @@ function parseConfig(entry: string, filepath: string) {
       console.log(config[0])
       configs.push(config[0])
     }
-    catch(err) {
+    catch (err) {
       console.log("not that declaration");
     }
   }
@@ -253,7 +253,7 @@ function parseConfig(entry: string, filepath: string) {
   // Add plugins
   // List of plugins
   // Assume first plugin
-  const plugins = [{description:"The SplitChunks plugin facilitates breaking modules into separate or combined files.", name:"Split Chunks plugin", file:"splitChunksPluginConfig.js"}]
+  const plugins = [{ description: "The SplitChunks plugin facilitates breaking modules into separate or combined files.", name: "Split Chunks plugin", file: "splitChunksPluginConfig.js" }]
   let plugin = plugins[0]
 
   fs.readFile(__dirname + "/../src/plugins/" + plugin.file, (err, data) => {  // todo: needs to be the plugins directory
@@ -266,7 +266,7 @@ function parseConfig(entry: string, filepath: string) {
     // merge plugin config with selected webpack config
   });
 
-  
+
   // Format it into a code string
   var formattedCode = generate(ast, {
     comments: true,
@@ -276,16 +276,16 @@ function parseConfig(entry: string, filepath: string) {
   // Check it
   //console.log(entry === formattedCode ? 'It works!' : 'Something went wrong…')
 
-  fs.writeFile(filepath+"v2", formattedCode, (err) => {  //need to do better versioning / archiving
+  fs.writeFile(filepath + "v2", formattedCode, (err) => {  //need to do better versioning / archiving
     if (err) {
-  //    alert("An error ocurred updating the file" + err.message);
+      //    alert("An error ocurred updating the file" + err.message);
       console.log(err);
       return;
     }
 
     console.log("The new file has been succesfully saved");
-  });  
-  
+  });
+
 }
 
 /**
@@ -293,65 +293,135 @@ function parseConfig(entry: string, filepath: string) {
  * Loading parsing of webpack stats file
  **/
 
-function selectStatsJson(){
+function selectStatsJson() {
   let file = dialog.showOpenDialog({ properties: ['openFile'] })[0]
   if (file != undefined) {
     loadStats(file)
   }
 }
 
+// function loadStats(file: string) {
+//   fs.readFile(file, (err, data) => {
+//     if (err) {
+//       //    alert("An error ocurred updating the file" + err.message); //alert doesn't work.
+//       console.log(err);
+//       return;
+//     }
+//     // clean and send back JSON stats file
+//     //let content = data.toString()
+//     let content: any = data.toString();
+
+//     //console.log(content)
+//     content = content.substr(content.indexOf("{"));
+
+//     //splits multiple JSON objects if more than one exists in file
+//     //content = content.split(/(?<=})[\n\r\s]+(?={)/)[1]  
+//     // content = "{" + content.split(/}[\n\r\s]+{/)[1]  
+//     content = content.split(/}[\n\r\s]+{/);
+//     // repair brackets from split
+
+//     console.log("content array length is",content.length)
+//     if (content.length > 1) {
+//       for (let i=0; i<content.length; i++){
+//         content[i] = (i>0)?"{":"" + content[i] + (i<content.length-1)?"}":""
+//       }
+//     }
+//     console.log("Stats File")
+//     console.log(content[0].substring(0,40))
+//     // console.log("Stats 2")
+//     // console.log(content[1].substring(0,40))
+//     // content is now an array of one or more stats json
+//     content = JSON.parse(content[0])
+//     while (!content.hasOwnProperty("builtAt")) {
+//       content = content.children[0]
+//     }
+//     let returnObj: any = {};
+//     returnObj.timeStamp = Date.now();
+//     returnObj.time = content.time;
+//     returnObj.hash = content.hash;
+//     returnObj.errors = content.errors
+//     returnObj.size = content.assets.reduce((size: number , asset: any): void => size + asset.size, 0)
+//     returnObj.assets = content.assets.map((asset: any) => ({
+//       name: asset.name,
+//       chunks: asset.chunks,
+//       size: asset.size,
+//     }));
+
+//     returnObj.chunks = content.chunks.map((chunk: any) => ({
+//       size: chunk.size,
+//       files: chunk.files,
+//       modules: chunk.modules ?
+//         chunk.modules.map((module: any) => ({
+//           name: module.name,
+//           size: module.size,
+//           id: module.id,
+//         }))
+//         : [],
+//     }));
+
+//     let Pdata: any = []
+//     Pdata.push(returnObj)
+//     //loops through assets
+//     let i = 0; // or the latest build
+//     let path: string;
+//     let sizeStr: string;
+//     let sunBurstData = [];
+
+
+//     for (var k = 0; k < Pdata[i].chunks.length; k++) {
+//       for (var l = 0; l < Pdata[i].chunks[k].modules.length; l++) {
+//         sizeStr = Pdata[i].chunks[k].modules[l].size.toString();
+//         path = Pdata[i].chunks[k].modules[l].name.replace("./", "");
+//         sunBurstData.push([path, sizeStr])
+//       }
+//     }
+//     const sunBurstDataSum: number = sunBurstData.reduce((sum: number, el: any): number => {
+//       return sum += parseInt(el[1])
+//     }, 0)
+
+//     console.log(sunBurstDataSum)
+//     //console.log(co)
+//     // console.log(content.substring(0, 40))
+//     mainWindow.webContents.send('display-stats-reply', sunBurstData)
+
+//     //mainWindow.webContents.send('display-stats-reply', JSON.parse(content))
+//   });
+// }
+
 function loadStats(file: string) {
   fs.readFile(file, (err, data) => {
     if (err) {
-      //    alert("An error ocurred updating the file" + err.message); //alert doesn't work.
       console.log(err);
       return;
     }
-    // clean and send back JSON stats file
-    //let content = data.toString()
-    let content: any = data.toString();
 
-    //console.log(content)
+    let content = data.toString();
     content = content.substr(content.indexOf("{"));
 
     //splits multiple JSON objects if more than one exists in file
-    //content = content.split(/(?<=})[\n\r\s]+(?={)/)[1]  
-    // content = "{" + content.split(/}[\n\r\s]+{/)[1]  
-    content = content.split(/}[\n\r\s]+{/);
-    // repair brackets from split
-
-    console.log("content array length is",content.length)
-    if (content.length > 1) {
-      for (let i=0; i<content.length; i++){
-        content[i] = (i>0)?"{":"" + content[i] + (i<content.length-1)?"}":""
-      }
-    }
-    console.log("Stats File")
-    console.log(content[0].substring(0,40))
-    // console.log("Stats 2")
-    // console.log(content[1].substring(0,40))
-    // content is now an array of one or more stats json
-    content = JSON.parse(content[0])
+    content = content.split(/(?<=})[\n\r\s]+(?={)/)[1]
+    content = JSON.parse(content)
+    //let content1 = JSON.parse(content)
     while (!content.hasOwnProperty("builtAt")) {
       content = content.children[0]
     }
-    let returnObj: any = {};
+    let returnObj = {};
     returnObj.timeStamp = Date.now();
     returnObj.time = content.time;
     returnObj.hash = content.hash;
     returnObj.errors = content.errors
-    returnObj.size = content.assets.reduce((size: number , asset: any): void => size + asset.size, 0)
-    returnObj.assets = content.assets.map((asset: any) => ({
+    returnObj.size = content.assets.reduce((size: number, asset: any): void => size + asset.size, 0)
+    returnObj.assets = content.assets.map(asset => ({
       name: asset.name,
       chunks: asset.chunks,
       size: asset.size,
     }));
 
-    returnObj.chunks = content.chunks.map((chunk: any) => ({
+    returnObj.chunks = content.chunks.map(chunk => ({
       size: chunk.size,
       files: chunk.files,
       modules: chunk.modules ?
-        chunk.modules.map((module: any) => ({
+        chunk.modules.map(module => ({
           name: module.name,
           size: module.size,
           id: module.id,
@@ -378,6 +448,8 @@ function loadStats(file: string) {
     const sunBurstDataSum: number = sunBurstData.reduce((sum: number, el: any): number => {
       return sum += parseInt(el[1])
     }, 0)
+
+
 
     console.log(sunBurstDataSum)
     //console.log(co)
