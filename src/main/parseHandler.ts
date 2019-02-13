c3570b5bfe3efcd328b3ce05c5ac3297520ab6ad
Dave Marquess <dave.marquess@gmail.com>
2019-02-13 00:13:46 -0700

    remove console.logs from parseHandler.ts

--- src/main/parseHandler.ts
+++ src/main/parseHandler.ts
@@ -5,9 +5,6 @@ import { generate } from 'astring';
 const prettier = require("prettier");
 import { any, string } from 'prop-types';
 import { exec } from 'child_process';
-// import { dirObj } from './main';
-
-//let directoryGlobal;
 
 interface ParseHandler {
   directory?: string,
@@ -49,19 +46,9 @@ interface ParseHandler {
 
   loadPlugin: (pluginName: string) => void;
 
-  //  loadPluginSplitChunks: () => void;
-
-  //  loadPluginMini: () => void;
-
   parsePlugin: (entry: string) => { pluginEntryPoints: any, ast: any };
 
-  //  parsePluginSplitChunks: (entry: string) => { entryPoints: any, ast: any };
-
   mergePlugin: () => void;
-
-  // addOptimizationSection: (ast: any) => void;
-
-  // addPluginsSection: (ast: any) => void;
 
   loadStats2: () => void;
 
@@ -129,15 +116,12 @@ const parseHandler: ParseHandler = {
   },
 
   parseConfig: function (entry: string, filepath: string, writeFile: boolean = false) {
-    console.log("doing parseConfig converts config to AST")
 
     let splitPoint: number = filepath.includes("/") ?
       filepath.lastIndexOf("/") :
       filepath.lastIndexOf("\\");
 
     this.directory = filepath.substring(0, splitPoint + 1);
-    console.log("filepath is set to", filepath)
-    console.log("this.directory is set to", this.directory)
 
     this.configFile = filepath.substring(splitPoint + 1);
 
@@ -147,61 +131,51 @@ const parseHandler: ParseHandler = {
   },
 
   initEntryPoints: function (entry: string = parseHandler.originalConfig, writeFile: boolean = false) {
-    console.log("initializing EntryPoints. Resets config to original.")
-
     // Parse it into an AST and retrieve the list of comments
     const comments: Array<string> = []
     var ast = acorn.parse(entry, {
       ecmaVersion: 6,
       locations: true,
       onComment: comments,
-    })
-    // console.log(JSON.stringify(ast))
+    });
     // Attach comments to AST nodes
-    astravel.attachComments(ast, comments)
+    astravel.attachComments(ast, comments);
 
     // Optionally writing ast to disk for testing purposes
     if (writeFile) {
       fs.writeFile(this.directory + "config.ast.json", JSON.stringify(ast, null, 2), (err) => {
-        console.log("The ast file has been succesfully saved");
       });
     }
 
     entryPoints.all = ast;
     entryPoints.body = ast.body;
-    // console.log(ast.body)
-    let i = ast.body.length - 1 // checking for module.exports starting from the end. Should be the last node.
-    let configs: any = []
-    let oldModuleExports: any
 
-    // console.log(i)
+    let i = ast.body.length - 1; // checking for module.exports starting from the end. Should be the last node.
+    let configs: any = [];
+    let oldModuleExports: any;
+
     while (i >= 0) {
-      let candidate = ast.body[i].expression
+      let candidate = ast.body[i].expression;
       if (
         candidate.left.object && candidate.left.object.name === "module" &&
         candidate.left.property && candidate.left.property.name === "exports"
       ) {
-        oldModuleExports = candidate.right
-        break
+        oldModuleExports = candidate.right;
+        break;
       }
-      i--
+      i--;
     }
-    // console.log('ken')
-    // console.log('en')
-
-
     // If the last element is module.exports, which it should be, then check
     // if it's an Object we have found the config object. 
     // If it's an array, we need to find the config objects.
     // Multi configs not supported right now
 
     if (oldModuleExports.type === "ObjectExpression") { // we've found the single config
-      console.log("we've found the single config")
-      // entryPoints.oldModuleExports.push(oldModuleExports)
+
       configs.push(oldModuleExports)
       entryPoints.moduleExports = oldModuleExports
     } else if (oldModuleExports.type === "ArrayExpression") { // there are multiple configs
-      console.log("gathering multiple configs")
+
       // not supported now
       let configNames = oldModuleExports.elements;
 
@@ -215,7 +189,6 @@ const parseHandler: ParseHandler = {
         })[0]
         configs.push(config.right)
       }
-      console.log("The number of configs is: ", configs.length)
     }
 
     // should support adding multiple config's plugin sections, currently does the first
@@ -243,14 +216,10 @@ const parseHandler: ParseHandler = {
 
     this.updatedConfig = formattedCode
 
-    console.log("updating config")
-    console.log(this.updatedConfig)
-
     return this.updatedConfig;
   },
 
   saveConfig: function () {
-    console.log("doing save config")
     let archiveName: string = this.configFile.split(".js")[0] + ".bak" + ".js"
 
     fs.rename(this.directory + this.configFile, this.directory + archiveName, (err) => {
@@ -263,14 +232,8 @@ const parseHandler: ParseHandler = {
         return;
       }
 
-      console.log("The new file has been succesfully saved as");
-      console.log(this.directory + this.configFile)
-      console.log("Old file has been archived as");
-      console.log(this.directory + archiveName)
     });
-
     this.loadStats2();
-
   },
 
   loadStats2: function () {
@@ -287,31 +250,21 @@ const parseHandler: ParseHandler = {
       });
     }
 
-    // console.log("calling runWebpack")
-    // console.log("this.directory: ", this.directory)
-    // console.log("this.configFile: ", this.configFile)
-    // console.log('selected config ------:', this.selectedConfig);
-
-    // let aPromise = runWebpack2("cd '" + this.directory + "' && webpack --config ./webpack.config.js --env production --profile --json > stats.json")
     let aPromise = runWebpack2("cd '" + this.directory + "' && " + this.selectedConfig)
       .then((res) => {
-        console.log("there was a response")
-        isStatsUpdated()
+        isStatsUpdated();
         // go display webpack stats
       })
       .catch((err) => {
-        console.log("there was an error")
-        console.log(err)
-      })
+        console.log(err);
+      });
 
     function isStatsUpdated() {
-      console.log("isStatsUpdated?")
       fs.readFile("stats.json", (err, data) => {
         if (err) {
           console.log(err);
           return;
         }
-        console.log((data.toString()));
       });
     }
   },
@@ -320,30 +273,22 @@ const parseHandler: ParseHandler = {
 
     let pluginFileName = ""
     if (name === "Moment") {
-      console.log("doing Moment")
-      pluginFileName = "momentIgnorePluginConfig.js"
+      pluginFileName = "momentIgnorePluginConfig.js";
 
     } else if (name === "SplitChunks") {
-      console.log("doing Split Chunks")
-      pluginFileName = "splitChunksPluginConfig.js"
+      pluginFileName = "splitChunksPluginConfig.js";
 
     } else if (name === "mini") {
-      console.log("not doing Mini")
-      return
+      return;
     }
     fs.readFile(__dirname + '/../src/plugins/' + pluginFileName, (err, data) => {
       if (err) return console.log(err)
       this.parsePlugin(data.toString())
-      // console.dir("^^^^^^^^^^^^^^^console.dir^^^^^^^")
-      // console.dir(JSON.stringify(entryPoints.all, null, 2))
       return { entryPoints: pluginEntryPoints }
-
     });
   },
 
   parsePlugin: function (entry: string) {
-    console.log("Doing parsePlugin. Converts plugin into AST")
-
     // Parse it into an AST and retrieve the list of comments
     const comments: Array<string> = []
     const ast = acorn.parse(entry, {
@@ -364,19 +309,18 @@ const parseHandler: ParseHandler = {
 
     // loop through ast looking for module.exports
     while (i >= 0) {
-      let candidate = ast.body[i].expression
+      let candidate = ast.body[i].expression;
       if (
         candidate.left.object && candidate.left.object.name === "module" &&
         candidate.left.property && candidate.left.property.name === "exports"
       ) {
-        oldModuleExports = candidate.right
-        break
+        oldModuleExports = candidate.right;
+        break;
       }
-      i--
+      i--;
     }
 
     if (oldModuleExports.type === "ObjectExpression") {
-      // pluginEntryPoints.oldModuleExports.push(oldModuleExports)
       configs.push(oldModuleExports)
       pluginEntryPoints.moduleExports = oldModuleExports
     }
@@ -388,30 +332,24 @@ const parseHandler: ParseHandler = {
     this.mergePlugin()
 
     return { pluginEntryPoints, ast }
-
   },
 
 
   definePlugins: function (plugins: Array<AvailablePlugin>) { // array of plugins
-    this.plugins = plugins
+    this.plugins = plugins;
   },
 
   showPlugins: function () {
-    return this.plugins
+    return this.plugins;
   },
 
   mergePlugin: function () {
-    console.log("going to do the merge %%%%%%%%%%%%%%%%%%%%")
-
-    console.log("add variable declarations - 'require' statements")
     // Add any variable declarations to the top of config
     // todo: Add multiple variable declarations from plugin 
     if (pluginEntryPoints.body[0].type === "VariableDeclaration") {
       entryPoints.body.unshift(pluginEntryPoints.body[0])  // should check to see if already exists 
       // and do all variable definitions. currently doing one.
     }
-
-    console.log("add plugins")
     // Add any plugins to the plugins section of the config
     if (pluginEntryPoints.pluginsSection && pluginEntryPoints.pluginsSection.value.elements.length !== 0) {
       // check to see if plugins section of config exists and add if needed
@@ -425,74 +363,21 @@ const parseHandler: ParseHandler = {
         })
       }
     }
-
-    console.log("add optimizations")
     // Add any optimizations to the optimizations section of the config
     if (pluginEntryPoints.optimizationSection && pluginEntryPoints.optimizationSection.value.properties.length !== 0) {
       // check to see if optimization section of config exists and add if needed
       if (!entryPoints.optimizationSection) {
         // check to see if there is an optimization section already in place  
         entryPoints.moduleExports.properties.push(pluginEntryPoints.optimizationSection)
-        // console.log("** Added plugins opts **********************************")
-        // console.log("length ME  = ",entryPoints.moduleExports.properties.length)
-        // console.log("length bdy = ",entryPoints.body[1].expression.right.properties.length)
-        // console.log("length all = ",entryPoints.all.body[1].expression.right.properties.length)
       } else {
         pluginEntryPoints.optimizationSection.value.properties.forEach(element => {
           entryPoints.optimizationSection.value.properties
             .push(JSON.parse(JSON.stringify(element)))
-        })
+        });
       }
     }
-
     this.updateConfig()
   },
-
-  // addOptimizationSection: function (ast) {
-  //   entryPoints.moduleExports[0].properties.push(
-  //     {
-  //       "type": "Property",
-  //       "method": false,
-  //       "shorthand": false,
-  //       "computed": false,
-  //       "loc": null,
-  //       "key": {
-  //         "type": "Identifier",
-  //         "name": "optimization"
-  //       },
-  //       "value": {
-  //         "type": "ObjectExpression",
-  //         "properties": [
-  //         ]
-  //       },
-  //       "kind": "init"
-  //     }
-  //   )
-
-  //   entryPoints.optimizationSection = entryPoints.moduleExports[0].properties.filter(element => element.key.name === "optimization")[0]
-  // },
-
-  // addPluginsSection: function (ast) {
-  //   entryPoints.moduleExports[0].properties.push(
-  //     {
-  //       "type": "Property",
-  //       "method": false,
-  //       "shorthand": false,
-  //       "computed": false,
-  //       "key": {
-  //         "type": "Identifier",
-  //         "name": "plugins"
-  //       },
-  //       "value": {
-  //         "type": "ArrayExpression",
-  //         "elements": []
-  //       },
-  //       "kind": "init"
-  //     }
-  //   )
-
-  //   entryPoints.pluginsSection = entryPoints.moduleExports[0].properties.filter(element => element.key.name === "plugins")[0]
-  // },
 }
 
 export default parseHandler;
