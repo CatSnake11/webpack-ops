941ad4295d69e1daa7d66d4caf4675bb08b02599
Dave Marquess <dave.marquess@gmail.com>
2019-02-18 12:26:42 -0700

    remove moment locale variables

--- src/plugins/momentLocalsPluginConfig.js
+++ src/plugins/momentLocalsPluginConfig.js
@@ -1,8 +1,8 @@
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
- 
-module.exports = {
-    plugins: [
-        // To strip all locales except “en”
-        new MomentLocalesPlugin(),
-    ],
-};
+// const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+// module.exports = {
+//     plugins: [
+//         // To strip all locales except “en”
+//         new MomentLocalesPlugin(),
+//     ],
+// };
